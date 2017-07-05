61fd38effa59d7eb0a9e67d58f3ae0f0b1c7774e
Ruben Thys <ruben.thys@student.odisee.be>
2017-07-05 12:23:07 +0200

    clean up js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
+(function() {
+
+    'use strict';
 
     $(document).ready(function(){
-    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
-    $('.modal').modal();
-      $('#modal1').modal('open');
-  });
+        $('.modal').modal();
+    });
+})();
