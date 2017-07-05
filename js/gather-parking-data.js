2f1b542dfaa122c7f874b30e054a152a421b5f08
Arne Gevaert <arne.gevaert@ugent.be>
2017-07-05 10:43:07 +0200

    fetch last 5 measurement files

--- js/gather-parking-data.js
+++ js/gather-parking-data.js
@@ -1,8 +1,35 @@
-const fetch = require('ldfetch');
+const ldfetch = require('ldfetch');
 const n3 = require('n3');
-const $ = require('jquery');
 const base_url = "http://linked.open.gent/parking/";
 
-function gather_data() {
-  return 5;
+function fetchUrl(url, amount, state) {
+  let fetch = new ldfetch();
+
+  return new Promise((resolve) => {
+    fetch.get(url).then(response => {
+      state.addTriples(response.triples);
+      state.addPrefixes(response.prefixes);
+      return response;
+    }).then(response => {
+      if (amount < 5) {
+        let responseStore = new n3.Store();
+        responseStore.addTriples(response.triples);
+        responseStore.addPrefixes(response.prefixes);
+
+        let prevUrl = responseStore.getTriples(null,"hydra:previous")[0].object;
+        console.log("previous: " + prevUrl);
+        return fetchUrl(prevUrl, amount+1, state);
+      } else {
+        return state;
+      }
+    }).then(res => resolve(res));
+  })
 }
+
+global.gatherData = function () {
+  fetchUrl(base_url, 0, new n3.Store()).then(res => {
+    console.log("Done!");
+    console.log(res);
+  });
+  return 100;
+};
