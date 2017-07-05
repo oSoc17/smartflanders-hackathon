1f05f3913fa1572326cc25eb412be9ba4d37962e
Arne Gevaert <arne.gevaert@ugent.be>
2017-07-05 09:58:03 +0200

    added jquery and test environment

--- js/gather-parking-data.js
+++ js/gather-parking-data.js
@@ -1,2 +1,8 @@
 const fetch = require('ldfetch');
 const n3 = require('n3');
+const $ = require('jquery');
+const base_url = "http://linked.open.gent/parking/";
+
+function gather_data() {
+  return 5;
+}
